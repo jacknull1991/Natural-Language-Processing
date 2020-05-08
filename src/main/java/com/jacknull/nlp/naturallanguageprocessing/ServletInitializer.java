ae4cc7e546ea7f91128f630e3e6935cc571e211f
jacknull1991 <xixiang.chen1991@outlook.com>
2020-05-08 14:38:48 -0700

    project setup by spring boot

---
+++ src/main/java/com/jacknull/nlp/naturallanguageprocessing/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.jacknull.nlp.naturallanguageprocessing;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(NaturalLanguageProcessingApplication.class);
+	}
+
+}
---
+++ src/test/java/com/jacknull/nlp/naturallanguageprocessing/NaturalLanguageProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacknull.nlp.naturallanguageprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaturalLanguageProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
